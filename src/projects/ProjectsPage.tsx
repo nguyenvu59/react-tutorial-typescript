5fee119aed0e2fa8f3b160e88c5e942514c80890
vunt <nguyenvu5994@gmail.com>
2023-03-26 09:22:33 +0700

    update step 5

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { MOCK_PROJECTS } from './MockProjects'
 
 export default function ProjectsPage() {
-  return (
-    <div>
-        <h1>Projects</h1>
-    </div>
-  )
-}
+    return (
+        <>
+            <h1>Projects</h1>
+            <pre>
+                {JSON.stringify(MOCK_PROJECTS, null, ' ')}
+            </pre>
 
+        </>
+    )
+}
