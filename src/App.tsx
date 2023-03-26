27eb008c9de6bb359ad68bcb63360814a9310f8b
vunt <nguyenvu5994@gmail.com>
2023-03-26 08:39:50 +0700

    update step 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
   return (
@@ -20,9 +21,12 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    // <blockquote cite="Benjamin Franklin">
+    //   Tell me and I forget, teach me and I may remember, involve me and I learn.
+    // </blockquote>
+    <div className='container'>
+      <ProjectsPage />
+    </div>
   );
 }
 
