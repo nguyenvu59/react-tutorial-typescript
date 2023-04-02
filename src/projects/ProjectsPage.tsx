b42eaa363508a416a5c24546102c05c840b161c0
nguyenvu59 <nguyenvu5994@gmail.com>
2023-04-02 16:28:44 +0700

    lab 6

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { MOCK_PROJECTS } from './MockProjects'
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
 
 export default function ProjectsPage() {
     return (
         <>
             <h1>Projects</h1>
-            <pre>
+            {/* <pre>
                 {JSON.stringify(MOCK_PROJECTS, null, ' ')}
-            </pre>
-
+            </pre> */}
+            <ProjectList projects={MOCK_PROJECTS} />
         </>
     )
 }
